a44bf556dda62322d4eebd27d332be6d9254db00
raphaelrl86 <raphael.lacerda@gmail.com>
2023-03-29 20:32:55 -0300

    done

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+    return (
+        user: {
+            name: "Thoughts of Dog®",
+            image: "https://i.imgur.com/b0EdHVV.jpg",
+            handle: "dog_feelings",
+          }
+    )
+}
